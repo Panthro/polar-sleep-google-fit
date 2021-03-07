8902a88c2d7c7aa8a1b9d2548bee7e6b8e849b5f
Rafael Roman <rafael.roman@n26.com>
2021-03-07 16:19:12 +0100

    adds current status use case

---
+++ src/test/kotlin/com/rafaelroman/application/currentstatus/CurrentStatusUseCaseTest.kt
@@ -0,0 +1,67 @@
+package com.rafaelroman.application.currentstatus
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import com.rafaelroman.domain.googlefit.GoogleAccessTokenRepository
+import com.rafaelroman.domain.polar.PolarAccessTokenRepository
+import com.rafaelroman.fixtures.buildGoogleAccessToken
+import com.rafaelroman.fixtures.buildPolarAccessToken
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Test
+
+internal class CurrentStatusUseCaseTest {
+
+
+    @Test
+    fun `should return google authenticated and polar authenticated`() = runBlocking{
+        // Arrange
+        val googleAccessTokenRepository: GoogleAccessTokenRepository = mockk()
+        val polarAccessTokenRepository: PolarAccessTokenRepository = mockk()
+        val useCase = CurrentStatusUseCase(googleAccessTokenRepository, polarAccessTokenRepository)
+
+        coEvery {
+            googleAccessTokenRepository.current()
+        } returns buildGoogleAccessToken()
+
+        coEvery {
+            polarAccessTokenRepository.current()
+        } returns buildPolarAccessToken()
+
+        // Act
+        val result = useCase.status()
+        // Assert
+        assertThat(result).isEqualTo(
+            CurrentStatus(
+                googleAuthStatus = GoogleAuthStatus.Authenticated,
+                polarAuthStatus = PolarAuthStatus.Authenticated
+            )
+        )
+    }
+    @Test
+    fun `should return google unauthenticated and polar unauthenticated`() = runBlocking{
+        // Arrange
+        val googleAccessTokenRepository: GoogleAccessTokenRepository = mockk()
+        val polarAccessTokenRepository: PolarAccessTokenRepository = mockk()
+        val useCase = CurrentStatusUseCase(googleAccessTokenRepository, polarAccessTokenRepository)
+
+        coEvery {
+            googleAccessTokenRepository.current()
+        } returns null
+
+        coEvery {
+            polarAccessTokenRepository.current()
+        } returns null
+
+        // Act
+        val result = useCase.status()
+        // Assert
+        assertThat(result).isEqualTo(
+            CurrentStatus(
+                googleAuthStatus = GoogleAuthStatus.Unauthenticated,
+                polarAuthStatus = PolarAuthStatus.Unauthenticated
+            )
+        )
+    }
+}

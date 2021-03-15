93455ea1acb2bab161834c429ffbc790424d1b92
Rafael Roman <rafael.roman@n26.com>
2021-03-15 22:38:40 +0100

    launch Google sleep write async with

--- src/main/kotlin/com/rafaelroman/application/syncpolarsleepdata/SyncPolarSleepDataUseCase.kt
+++ src/main/kotlin/com/rafaelroman/application/syncpolarsleepdata/SyncPolarSleepDataUseCase.kt
@@ -4,6 +4,8 @@ import com.rafaelroman.domain.googlefit.GoogleAccessTokenRepository
 import com.rafaelroman.domain.googlefit.GoogleFitSleepNightPublisher
 import com.rafaelroman.domain.polar.PolarAccessTokenRepository
 import com.rafaelroman.domain.polar.PolarSleepDataProvider
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import org.slf4j.LoggerFactory
 
 private val logger = LoggerFactory.getLogger(SyncPolarSleepDataUseCase::class.java)
@@ -12,18 +14,22 @@ class SyncPolarSleepDataUseCase(
     private val polarAccessTokenRepository: PolarAccessTokenRepository,
     private val polarSleepDataProvider: PolarSleepDataProvider,
     private val googleFitSleepPublisher: GoogleFitSleepNightPublisher,
-    private val googleAccessTokenRepository: GoogleAccessTokenRepository
+    private val googleAccessTokenRepository: GoogleAccessTokenRepository,
 ) {
 
     suspend fun sync(userId: String): SyncPolarSleepDataSuccessfully {
-        polarAccessTokenRepository.find(userId).let { accessToken ->
-            polarSleepDataProvider latest accessToken!!
-        }.apply {
-            logger.info("process=sync-polar-nights status=nights-returned nights=${this.size}")
-        }.forEach {
-            googleFitSleepPublisher publish (it to googleAccessTokenRepository.find(userId)!!)
-            logger.info("process=sync-polar-nights status=sync-night night=$it")
-        }.also {
+        coroutineScope {
+            val polarAccessToken = polarAccessTokenRepository.find(userId)!!
+            val googleAccessToken = googleAccessTokenRepository.find(userId)!!
+
+            val nights = polarSleepDataProvider latest polarAccessToken
+            logger.info("process=sync-polar-nights status=nights-returned nights=${nights.size}")
+            nights.map { night ->
+                async {
+                    googleFitSleepPublisher publish (night to googleAccessToken)
+                    logger.info("process=sync-polar-nights status=sync-night night=$night")
+                }
+            }.forEach { it.await() }
             logger.info("process=sync-polar-nights status=finished")
         }
         return SyncPolarSleepDataSuccessfully
